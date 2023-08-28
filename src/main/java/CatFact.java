0c41ae24ebd8fbe57b069587238baca5a0fc9ef2
pavelkurtukov <pavel@kurtukov.ru>
2023-08-29 00:33:29 +0700

    Initial commit

---
+++ src/main/java/CatFact.java
@@ -0,0 +1,53 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CatFact {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upvotes;
+
+    public CatFact(
+            @JsonProperty("id")
+            String id,
+            @JsonProperty("text")
+            String text,
+            @JsonProperty("type")
+            String type,
+            @JsonProperty("user")
+            String user,
+            @JsonProperty("upvotes")
+            int upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("КотоФакт #%s [%s]: \"%s\". Автор: %s. Проголовало: %d", id, type, text, user, upvotes);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Main {
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        CloseableHttpResponse response = httpClient.execute(request);
+        InputStream content = (response.getEntity().getContent());
+
+        // Преобразуем поток ответа в список объектов CatFact
+        List<CatFact> catFacts = mapper.readValue(content, new TypeReference<List<CatFact>>() {});
+
+        // Выводим на экран все факты, за которые хоть кто-нибудь проголосовал
+        catFacts.stream().filter(catFact -> catFact.getUpvotes() > 0).forEach(System.out::println);
+    }
+}
